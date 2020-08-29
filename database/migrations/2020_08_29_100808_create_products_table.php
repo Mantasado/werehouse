fe2425d3fc6edce3e6467e13329ecf54b0dc2e90
Mantas Adomaitis <mantas94a@gmail.com>
2020-08-29 14:31:39 +0300

    Structured database via migrations

---
+++ database/migrations/2020_08_29_100808_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->integer('ean', 13);
+            $table->foreignId('product_type_id');
+            $table->string('color', 20);
+            $table->boolean('active')->default(0);
+            $table->string('image', 100);
+            $table->foreignId('product_details_id');
+            $table->boolean('is_deleted')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
